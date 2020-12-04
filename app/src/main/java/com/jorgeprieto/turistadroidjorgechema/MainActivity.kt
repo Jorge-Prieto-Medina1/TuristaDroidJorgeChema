fea629e3c92cc1022b5fa3bf69e9096b200b2f46
Jorge <jorgepri00@gmail.com>
2020-12-04 12:09:52 +0100

    Commit Login Registro
    Commit de las pantallas login y registro

--- app/src/main/java/com/jorgeprieto/turistadroidjorgechema/MainActivity.kt
+++ app/src/main/java/com/jorgeprieto/turistadroidjorgechema/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.jorgeprieto.turistadroidjorgechema
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
