2f91da9734ed446095e28def9c1834317914df95
Chema <Chemaruiz94@gmail.com>
2020-12-09 21:09:37 +0100

    Refactor 1.4

--- app/src/main/java/com/jorgeprieto/turistadroidjorgechema/App/Session.kt
+++
@@ -1,6 +0,0 @@
-package com.jorgeprieto.turistadroidjorgechema.App
-
-import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
-
-open class Session (@PrimaryKey var emailUser : String = "") : RealmObject()
