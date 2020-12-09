d5352098bbd82761c2b615f0c97321678504d641
Chema <Chemaruiz94@gmail.com>
2020-12-09 20:17:14 +0100

    Session 1.1

--- app/src/main/java/com/jorgeprieto/turistadroidjorgechema/App/Session.kt
+++ app/src/main/java/com/jorgeprieto/turistadroidjorgechema/App/Session.kt
@@ -3,6 +3,4 @@ package com.jorgeprieto.turistadroidjorgechema.App
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-open class Sesion {
-    open class Session (@PrimaryKey var emailUser : String = "") : RealmObject()
-}
+open class Session (@PrimaryKey var emailUser : String = "") : RealmObject()
