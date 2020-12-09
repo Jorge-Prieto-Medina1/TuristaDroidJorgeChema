2d196fa127cb5c07869fa08cfd440d0e2d31f41d
Chema <Chemaruiz94@gmail.com>
2020-12-09 18:35:13 +0100

    Commit Lugares 1.1

---
+++ app/src/main/java/com/jorgeprieto/turistadroidjorgechema/App/Image.kt
@@ -0,0 +1,9 @@
+package com.jorgeprieto.turistadroidjorgechema.App
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Image (@PrimaryKey var id : Long = 0,
+                  var foto : String = "") : RealmObject(){
+    constructor( foto : String) : this((System.currentTimeMillis() / 1000L), foto)
+}
--- app/src/main/java/com/jorgeprieto/turistadroidjorgechema/lugares/Lugar.kt
+++ app/src/main/java/com/jorgeprieto/turistadroidjorgechema/lugares/Lugar.kt
@@ -2,38 +2,19 @@ package com.jorgeprieto.turistadroidjorgechema.lugares
 
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-import io.realm.annotations.RealmClass
-import io.realm.annotations.Required
 import java.util.*
+import io.realm.RealmList
+import com.jorgeprieto.turistadroidjorgechema.App.Image
 
-@RealmClass
-open class Lugar (
+open class Place (@PrimaryKey var id : Long = 0,
+                  var nombre : String = "",
+                  var fecha : Date = Date(),
+                  var city : String = "",
+                  var puntuacion : Double = 0.0,
+                  var longitud : Double = 0.0,
+                  var latitud : Double = 0.0,
+                  var imagenes : RealmList<Image> = RealmList()) : RealmObject(){
+    constructor(nombre: String, fecha: Date,city: String, puntuacion: Double, longitud: Double, latitud: Double, imagenes: RealmList<Image>) :
+            this((System.currentTimeMillis() / 1000L), nombre, fecha, city, puntuacion, longitud, latitud, imagenes)
+}
 
-        @PrimaryKey var id: String = "",
-        @Required var nombre: String = "",
-        @Required var tipo: String = "Poblacion",
-        @Required var fecha: String = "",
-        @Required var latitud: String = "",
-        @Required var longitud: String = "",
-        var valoracion: Int = 0,
-        var favorito: Boolean = false,
-        var usuarioID: String = ""
-    ) : RealmObject() {
-
-        constructor(
-            nombre: String,
-            tipo: String,
-            fecha: String,
-            latitud: String,
-            longitud: String,
-            valoracion: Int,
-            favorito: Boolean,
-            usuarioID: String
-        ) : this((UUID.randomUUID().toString()), nombre, tipo, fecha, latitud, longitud, valoracion, favorito, usuarioID)
-
-        override fun toString(): String {
-            return "Lugar(id='$id', nombre='$nombre', tipo='$tipo', fecha='$fecha', latitud='$latitud', longitud='$longitud', valoracion=$valoracion, favorito=$favorito, usuarioID='$usuarioID')"
-        }
-
-
-    }
