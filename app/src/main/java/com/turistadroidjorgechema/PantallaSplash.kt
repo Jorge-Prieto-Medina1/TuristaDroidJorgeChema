5273880c9dd67a4300010830e42c1b030b17c2f0
Chema <Chemaruiz94@gmail.com>
2020-12-09 20:17:14 +0100

    Session 1.1

--- app/src/main/java/com/turistadroidjorgechema/PantallaSplash.kt
+++ app/src/main/java/com/turistadroidjorgechema/PantallaSplash.kt
@@ -3,14 +3,18 @@ package com.turistadroidjorgechema
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import android.view.animation.*
 import androidx.constraintlayout.motion.widget.MotionLayout
+import com.jorgeprieto.turistadroidjorgechema.App.SessionController
 import kotlinx.android.synthetic.main.activity_pantalla_splash.*
 
 
 class PantallaSplash : AppCompatActivity() {
 
+    private val TIME : Long = 5000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,4 +43,6 @@ class PantallaSplash : AppCompatActivity() {
         })
 
     }
+
+
 }
